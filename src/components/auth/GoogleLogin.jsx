38c1d16ba29909c32a409b22114c344e99a8ea4d
RISHU RAJ <RR907194@GMAIL.com>
2025-08-24 13:01:15 +0530

    commit

--- src/components/auth/GoogleLogin.jsx
+++ src/components/auth/GoogleLogin.jsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function GoogleLogin() {
   const handleGoogleLogin = () => {
     const apiBase = import.meta.env.VITE_API_URL || "http://localhost:5001";
-const googleAuthUrl = `${apiBase.replace(/\/+$/, "")}/auth/google`;
+    const googleAuthUrl = `${apiBase.replace(/\/+$/, "")}/api/auth/google`;
 window.location.href = googleAuthUrl;
   };
 
